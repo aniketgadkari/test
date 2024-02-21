9a74051d4e9e254c50b6c835c290adbc9439e4e9
aniketgadkari <40064400+aniketgadkari@users.noreply.github.com>
2024-02-20 23:59:11 -0500

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,3 +1,4 @@
+/*
 output "sec_grp_arn" {
     value = module.sec_grp.endpoint_sg_arn
 }
@@ -9,10 +10,9 @@ output "sec_grp_id" {
 output "endpoint_servicename" {
     value = "This is the endpoint created ${module.end_point.endpoint}"
 }
+*/
 
-/*
 output "endpoint" {
     value = aws_vpc_endpoint.ec2.service_name
   
 }
-*/
